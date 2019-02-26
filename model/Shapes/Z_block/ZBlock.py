cf79472d17c8600cb753d5eb30a624514e8d8c4f
Milosz Blasiak <milblasiak@gmail.com>
2019-02-26 19:22:13 +0100

    Add ZBlock with all states

---
+++ model/Shapes/Z_block/ZBlock.py
@@ -0,0 +1,17 @@
+from model.Box import Box
+from model.Shapes.Block import Block
+from model.Shapes.Z_block.States import DownState
+
+
+class ZBlock(Block):
+
+    def __init__(self, game_manager, app, map_controller, center_x, center_y):
+        self.a = Box(game_manager, center_x - 1, center_y + 1, app, map_controller)
+        self.b = Box(game_manager, center_x, center_y + 1, app, map_controller)
+        self.c = Box(game_manager, center_x, center_y, app, map_controller)
+        self.d = Box(game_manager, center_x + 1, center_y, app, map_controller)
+        super().__init__([self.a, self.b, self.c, self.d], DownState(self), center_x, center_y, game_manager)
+
+    def rotate(self):
+        if self.can_spin():
+            self.state.rotate()
