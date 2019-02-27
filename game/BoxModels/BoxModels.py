bd9d99ecb789b542bad27968795a12251a0ade68
Milosz Blasiak <milblasiak@gmail.com>
2019-02-27 22:51:27 +0100

    Add model switcher

--- game/BoxModels/BoxModels.py
+++ game/BoxModels/BoxModels.py
@@ -8,3 +8,16 @@ class BoxModel:
 class WoodenBox(BoxModel):
     def __init__(self):
         super().__init__("wooden","resources/PS2.egg")
+
+
+class ModelSwitcher:
+
+    def __init__(self):
+        self.switcher_dict = {
+            1: WoodenBox,
+        }
+
+    def number_to_model(self, number):
+        if number not in self.switcher_dict:
+            return self.switcher_dict[1]
+        return self.switcher_dict[number]
