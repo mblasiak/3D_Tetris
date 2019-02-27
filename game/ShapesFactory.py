51a1d1f7155af2da5ef57c823725791b45dcdd2c
Milosz Blasiak <milblasiak@gmail.com>
2019-02-27 22:54:07 +0100

    Add usage of model slither in ShapeFactory

--- game/ShapesFactory.py
+++ game/ShapesFactory.py
@@ -1,5 +1,7 @@
-from game.BoxModels.BoxModels import WoodenBox
-from game.Shapes.I_block.IBlock import IBlock
+from random import randrange
+
+from game.BoxModels.BoxModels import WoodenBox, ModelSwitcher
+from game.Shapes.ShapeSwitcher import ShapeSwitcher
 
 
 class ShapesFactory:
@@ -10,6 +12,11 @@ class ShapesFactory:
         self.center_y = drop_y
         self.center_x = drop_x
         self.game_manager = game_manager
+        self.shape_switcher = ShapeSwitcher()
+        self.model_switcher = ModelSwitcher()
 
     def get_random_shape(self):
-        return IBlock(self.game_manager, self.app, self.map_controller, self.center_x, self.center_y, WoodenBox())
+        rand = randrange(1, 8, 1)
+        shape = self.shape_switcher.number_to_shape(rand)
+        model=self.model_switcher.number_to_model(rand)
+        return shape(self.game_manager, self.app, self.map_controller, self.center_x, self.center_y, model())
