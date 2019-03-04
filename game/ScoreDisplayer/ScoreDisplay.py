6dffdf715571d2acd7bdd64544f9c76a3a4fe459
Milosz Blasiak <milblasiak@gmail.com>
2019-03-04 01:51:34 +0100

    Add score holding to score display

--- game/ScoreDisplayer/ScoreDisplay.py
+++ game/ScoreDisplayer/ScoreDisplay.py
@@ -7,7 +7,8 @@ class ScoreDisplay:
         font = app.loader.loadFont('resources/fonts/Gobold Uplow Italic.ttf')
         self.text = OnscreenText(text="SCORE: 0", pos=(-0.7, +0.5), scale=0.09, fg=(1, 1, 1, 1))
         self.text.setFont(font)
+        self.score = 0
 
-    def update(self, score):
-        print("Update")
-        self.text.setText("SCORE: " + str(score))
+    def add_point(self):
+        self.score += 1
+        self.text.setText("SCORE: " + str(self.score))
